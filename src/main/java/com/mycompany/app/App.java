6b59343257fa4c0fe7aad1a4d45a8e7598dfc910
Keysight Jex <jex.khor@keysight.com>
2024-02-20 16:34:13 +0800

    Make a change to demo Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins Good! I will use it once a while maybe!");
+        System.out.println("Wew Jenkins Good! I will use it once a while maybe!");
     }
 
     public String getMessage() {
