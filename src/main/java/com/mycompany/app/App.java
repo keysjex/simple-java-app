ca58978067f2f19c942a91fe7013915157951ddd
Keysight Jex <jex.khor@keysight.com>
2024-02-21 11:03:17 +0800

    No prob here

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Testing out webhook feature, hopefully success hehe");
+        System.out.println("Testing out webhook feature, hopefully success hmm...");
     }
 
     public String getMessage() {
