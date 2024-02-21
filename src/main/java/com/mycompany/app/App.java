8eb5eec1df6961f1cd83fcca80fc482656789c1c
Keysight Jex <jex.khor@keysight.com>
2024-02-21 09:24:38 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Wew Jenkins Good! I will use it once a while maybe!");
+        System.out.println("Testing out webhook feature, hopefully success hehe");
     }
 
     public String getMessage() {
