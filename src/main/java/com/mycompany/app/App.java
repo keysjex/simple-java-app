ea385f74b84f563e7aefea068be4ca8b9c7c6e87
Keysight Jex <jex.khor@keysight.com>
2024-02-21 14:14:29 +0800

    test here 4

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Testing out webhook feature, hopefully success hmm...");
+        System.out.println("This is my latest change");
     }
 
     public String getMessage() {
